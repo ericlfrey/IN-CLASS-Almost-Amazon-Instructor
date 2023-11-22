a26c0c0dd4c4a1ce7cdf2a0baf3c61bef48bf093
Eric Frey <ericlfrey@gmail.com>
2023-11-22 16:59:22 -0600

    Caught up with class

--- api/authorData.js
+++ api/authorData.js
@@ -1,27 +1,60 @@
-// import client from '../utils/client';
+import client from '../utils/client';
 
-// const endpoint = client.databaseURL;
+const endpoint = client.databaseURL;
 
 // FIXME:  GET ALL AUTHORS
-const getAuthors = () => {};
+const getAuthors = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
 
 // FIXME: CREATE AUTHOR
-const createAuthor = () => {};
+const createAuthor = () => { };
 
 // FIXME: GET SINGLE AUTHOR
-const getSingleAuthor = () => {};
+const getSingleAuthor = () => { };
 
-// FIXME: DELETE AUTHOR
-const deleteSingleAuthor = () => {};
+// DELETE AUTHOR
+const deleteSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getFavoriteAuthors = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors.json?orderBy="favorite"&equalTo=true`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
 
 // FIXME: UPDATE AUTHOR
-const updateAuthor = () => {};
+const updateAuthor = () => { };
 
 // TODO: GET A SINGLE AUTHOR'S BOOKS
-const getAuthorBooks = () => {};
+const getAuthorBooks = () => { };
 
 export {
   getAuthors,
+  getFavoriteAuthors,
   createAuthor,
   getSingleAuthor,
   deleteSingleAuthor,
--- pages/books.js
+++ pages/books.js
@@ -15,17 +15,27 @@ const showBooks = (array) => {
   let domString = '';
   array.forEach((item) => {
     domString += `
-      <div class="card">
-        <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
-        <div class="card-body" style="height: 180px;">
-          <h5 class="card-title">${item.title}</h5>
-            <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
-            <hr>
-            <i class="btn btn-success fas fa-eye" id="view-book-btn--${item.firebaseKey}"></i>
-            <i id="edit-book-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-            <i id="delete-book-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
-        </div>
-      </div>`;
+    <div class="card">
+    <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
+    <div class="card-body book-card" style="height: 275px;">
+      <h6 class="card-title">${item.title}</h6>
+      <h6 class="card-title">${item.first_name} ${item.last_name}</h6>          
+      <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
+      <hr>
+      <div class="book-icons">
+        <i class="btn btn-success"   id="view-book-btn--${item.firebaseKey}">
+          <span><i class= "fas fa-eye"></i></span>
+        </i>
+        <i id="edit-book-btn--${item.firebaseKey}"  class="btn btn-info">
+          <span><i class= "fas fa-edit"></i></span>
+        </i>
+        <i id="delete-book-btn--${item.firebaseKey}"  class="btn btn-danger">
+          <span><i class= "fas fa-trash-alt"></i></span>
+        </i>
+      </div>
+    </div>
+  </div>
+  `;
   });
   renderToDOM('#store', domString);
 };
